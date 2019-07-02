39a16cbf09c9fdfea9cc9f1a02a792c3461c8602
irok <t.irokawa@gmail.com>
2019-07-02 12:34:33 +0900

    loadscriptを追加

---
+++ loadscript/bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:void(function(){var e=document.createElement("script");e.src="http://st-www.gmo.media/js/isbn/index.js?t="+Date.now(),e.async=!0;var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)}())
---
+++ loadscript/src.js
@@ -0,0 +1,7 @@
+(function(src) {
+  var script = document.createElement('script');
+  script.src = src + '?t=' + Date.now();
+  script.async = true;
+  var entry = document.getElementsByTagName('script')[0];
+  entry.parentNode.insertBefore(script, entry);
+})('http://st-www.gmo.media/js/isbn/index.js');
